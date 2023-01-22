8f5a83d29f94f04b131ec7ad11f6bca79ddafaf9
Vishal Choubey <74164476+VishalChoubey1019@users.noreply.github.com>
2023-01-23 00:31:04 +0530

    Update MyApp.js
    added previous and next buttons

--- src/pages/MyApp.js
+++ src/pages/MyApp.js
@@ -4,17 +4,26 @@ import { Document, Page, pdfjs } from 'react-pdf';
 export default function MyApp() {
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
-    const [selectedile, setFile] = useState(null);
-
+    const [selectedFile, setFile] = useState(null);
 
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
     }
-
+    
 
     React.useEffect(() => {
         pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
     }, [])
+
+    function nextButton(){
+        if (pageNumber < numPages) 
+            setPageNumber(pageNumber + 1);
+    }
+
+    function prevButton(){
+        if (pageNumber > 1) 
+            setPageNumber(pageNumber - 1);
+    }
 
     return (
         <div>
@@ -22,12 +31,16 @@ export default function MyApp() {
                 type="file"
                 onChange={(event) => setFile(event.target.files[0])}
             />
-            {selectedile ? <Document file={selectedile} onLoadSuccess={onDocumentLoadSuccess}>
-                <Page pageNumber={pageNumber} />
-            </Document> : <h1>Please upload a file</h1>}
-            <p>
-                Page {pageNumber} of {numPages}
-            </p>
+            {selectedFile ? 
+                <div>
+                    <Document file={selectedFile} onLoadSuccess={onDocumentLoadSuccess}>
+                        <Page pageNumber={pageNumber} />
+                    </Document>
+                    <p>Page {pageNumber} of {numPages}</p>
+                    <button onClick={prevButton}>Previous</button>
+                    <button onClick={nextButton}>Next</button>
+                </div>
+            : <h1>Please upload a file</h1>}
         </div>
     );
 }
