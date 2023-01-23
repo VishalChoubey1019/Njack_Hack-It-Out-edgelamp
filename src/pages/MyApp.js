3b6837bbd17bf6c174df15f5bee8036edff9429e
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2023-01-23 11:34:07 +0530

    added text and annotations

--- src/pages/MyApp.js
+++ src/pages/MyApp.js
@@ -1,29 +1,25 @@
 import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 export default function MyApp() {
     const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
+    const [currPage, setCurrPage] = useState(1);
     const [selectedFile, setFile] = useState(null);
 
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
+        setCurrPage(1);
     }
-    
+
+    function changePage(offset) {
+        setCurrPage(page => page + offset);
+    }
 
     React.useEffect(() => {
         pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
     }, [])
-
-    function nextButton(){
-        if (pageNumber < numPages) 
-            setPageNumber(pageNumber + 1);
-    }
-
-    function prevButton(){
-        if (pageNumber > 1) 
-            setPageNumber(pageNumber - 1);
-    }
 
     return (
         <div>
@@ -31,16 +27,16 @@ export default function MyApp() {
                 type="file"
                 onChange={(event) => setFile(event.target.files[0])}
             />
-            {selectedFile ? 
+            {selectedFile ?
                 <div>
                     <Document file={selectedFile} onLoadSuccess={onDocumentLoadSuccess}>
-                        <Page pageNumber={pageNumber} />
+                        <Page pageNumber={currPage} />
                     </Document>
-                    <p>Page {pageNumber} of {numPages}</p>
-                    <button onClick={prevButton}>Previous</button>
-                    <button onClick={nextButton}>Next</button>
+                    <p>Page {currPage} of {numPages}</p>
+                    {currPage > 1 && <button onClick={() => changePage(-1)}>Previous</button>}
+                    {currPage < numPages && <button onClick={() => changePage(1)}>Next</button>}
                 </div>
-            : <h1>Please upload a file</h1>}
+                : <h1>Please upload a file</h1>}
         </div>
     );
 }
