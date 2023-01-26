a2aa9180ca61906739c94ca58fc53e9d7260f54b
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2023-01-27 02:13:15 +0530

    changed meta data

--- src/context/CanvasContext.js
+++ src/context/CanvasContext.js
@@ -91,8 +91,9 @@ export const CanvasProvider = ({ children }) => {
     // add text
     const addText = canvi => {
         const text = new fabric.Textbox("Type Here ...", {
-            editable: true
+            editable: true,
         });
+        text.set({ fill: color })
         canvi.add(text);
         canvi.renderAll();
     }
